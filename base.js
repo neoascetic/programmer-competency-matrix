b61a7f0553f040e20567c37280d01afb51dce19a
Pavel Puchkin <neoascetic@gmail.com>
2013-06-13 22:04:05 +1100

    Cumulativeness! :D Fixes hltbra/programmer-competency-checklist#1 ;)

--- base.js
+++ base.js
@@ -1,31 +1,51 @@
 (function(body) {
 
+var CELLS_PER_ROW = 4;
 var STORAGE_KEY = 'matrix';
 var CHECKED_CLASS = 'checked';
 var CELLS = body.querySelectorAll('td:not(:first-child):not(:last-child)');
+
+body.addEventListener('click', function(e) {
+    var cell = e.target || [];
+    var cellIndex = Array.prototype.indexOf.call(CELLS, cell);
+    if (cellIndex === -1) return;
+
+    cell.classList.toggle(CHECKED_CLASS);
+
+    var isCellChecked = cell.classList.contains(CHECKED_CLASS);
+
+    var siblingGetterAction = isCellChecked ?
+        ['previousElementSibling', 'add'] :
+        ['nextElementSibling', 'remove'];
+    var sibling = cell[siblingGetterAction[0]];
+    while (sibling) {
+        sibling.classList[siblingGetterAction[1]](CHECKED_CLASS);
+        sibling = sibling[siblingGetterAction[0]];
+    }
+
+    var matrix = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    var rowIndex = Math.floor(cellIndex / CELLS_PER_ROW);
+    var relativeCellIndex = cellIndex % CELLS_PER_ROW;
+    if (isCellChecked) {
+        matrix[rowIndex] = relativeCellIndex;
+    } else if (relativeCellIndex !== 0) {
+        matrix[rowIndex] = relativeCellIndex - 1;
+    } else {
+        delete matrix[rowIndex];
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(matrix));
+});
 
 var matrix = JSON.parse(localStorage.getItem(STORAGE_KEY));
 if (!matrix) {
     matrix = [];
     localStorage.setItem(STORAGE_KEY, JSON.stringify(matrix));
 }
-Array.prototype.map.call(CELLS, function(cell, index) {
-    if (matrix[index]) cell.classList.add(CHECKED_CLASS);
+Array.prototype.map.call(matrix, function(maxCheckedCellRelativeIndex, rowIndex) {
+    if (maxCheckedCellRelativeIndex !== null) {
+        CELLS[rowIndex * CELLS_PER_ROW + maxCheckedCellRelativeIndex].click();
+    }
 });
 
-body.addEventListener('click', function(e) {
-    if (!e.target ||
-        e.target.tagName !== 'TD' ||
-        e.target.cellIndex < 1 ||
-        e.target.cellIndex > 4) return;
-    var cell = e.target;
-
-    cell.classList.toggle(CHECKED_CLASS);
-
-    var index = Array.prototype.indexOf.call(CELLS, cell);
-    var matrix = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    matrix[index] = cell.classList.contains(CHECKED_CLASS);
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(matrix));
-});
 
 })(document.getElementsByTagName('body')[0]);
